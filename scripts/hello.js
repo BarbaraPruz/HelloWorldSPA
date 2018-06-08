b23d841e21a3f3fb1479bc6839711f2eeb3276c9
barbara <barbara.prusiewicz@gmailcom>
2018-06-07 14:29:26 -1000

    updates to navigate to home, about, and contact (placeholder pages)

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,2 +1,17 @@
+function doNavigation (fn) {
+    let page = '../' + fn.substring(1) + '.html';
+    console.log ('doNavigation fn='+fn+' page='+page);
+    $('#mainContent').load(page);
+}
 
+$(window).on('hashchange', function(){
+    console.log( "hash change! " + window.location.hash);
+    doNavigation(window.location.hash);
+});
+$(document).ready(function() {  
+    let url = window.location.pathname;
+    let fn = url.slice(url.lastIndexOf("/")+1);
+    fn = fn ? fn : "#home"
+    doNavigation(fn)
+});  
   
